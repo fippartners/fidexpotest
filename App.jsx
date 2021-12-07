aee3872062f4bdd85037b8a9160677f1671dc372
fippartners <itsuki.0126.takada@gmail.com>
2021-12-07 14:31:04 +0900

    Create LogInScreen & SignUpScreen

--- App.jsx
+++ App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import LogInScreen from './src/screens/LogInScreen';
+import SigunUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
   return (
-    <MemoEditScreen />
+    <SigunUpScreen label="Welcome!" />
   );
 }
