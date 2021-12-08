c77d234f58735af6a97fb09700bee2797cffaad5
fippartners <itsuki.0126.takada@gmail.com>
2021-12-08 12:35:46 +0900

    add React Navigation

--- App.jsx
+++ App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoListScreen from './src/screens/MemoListScreen';
@@ -7,8 +9,14 @@ import MemoListScreen from './src/screens/MemoListScreen';
 // import LogInScreen from './src/screens/LogInScreen';
 // import SigunUpScreen from './src/screens/SignUpScreen';
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <MemoListScreen />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MemoListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
