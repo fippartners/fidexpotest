b83baeb43f2e3c3de090571a414aa6d40a36302d
fippartners <itsuki.0126.takada@gmail.com>
2021-12-09 11:08:37 +0900

    firebaseのエラー

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator, cardStyleInterpolator, CardStyleInterpolators } from '@react-navigation/stack';
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
+import firebase from 'firebase';
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
@@ -10,6 +11,18 @@ import LogInScreen from './src/screens/LogInScreen';
 import SigunUpScreen from './src/screens/SignUpScreen';
 
 const Stack = createStackNavigator();
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBM9dlgvm6BM0NAiT0EeY3NKhaKcNvWF_M',
+  authDomain: 'memoapp2-c5a11.firebaseapp.com',
+  projectId: 'memoapp2-c5a11',
+  storageBucket: 'memoapp2-c5a11.appspot.com',
+  messagingSenderId: '36722360622',
+  appId: '1:36722360622:web:69b779a89d64fb12eababf',
+};
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   return (
